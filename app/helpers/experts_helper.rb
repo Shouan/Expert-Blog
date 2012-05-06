ca7b50b0041d4bd61d87fa964ebbb05e23085a63
Shovan Swartz <Shovan.Swartz@Gmail.com>
2012-05-06 17:40:18 +0200

    Completed basis for sign up

--- app/helpers/experts_helper.rb
+++ app/helpers/experts_helper.rb
@@ -1,2 +1,8 @@
 module ExpertsHelper
+  # Returns the Gravatar (http://gravatar.com/) for the given user.
+  def gravatar_for(expert)
+    gravatar_id = Digest::MD5::hexdigest(expert.email_address.downcase)
+    gravatar_url = "http://gravatar.com/avatar/#{gravatar_id}.png"
+    image_tag(gravatar_url, alt: expert.first_name, class: "gravatar")
+  end
 end
